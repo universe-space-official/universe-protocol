a3ff57ce6a2f289753cc9416366b6a6b02283f6b
MartelAxe <martelaxe@gmail.com>
2022-09-29 11:46:48 -0500

    Adding return types orbis

--- src/modules/orbis/orbis.service.ts
+++ src/modules/orbis/orbis.service.ts
@@ -5,169 +5,169 @@ const orbis = new Orbis();
 
 export class OrbisService {
 
-    async connect(provider, lit) {
+    async connect(provider: any, lit: any): Promise<any> {
         await orbis.connect(provider, lit);
     }
 
-    async isConnected() {
+    async isConnected(): Promise<any> {
         return await orbis.isConnected();
     }
 
-    async logout() {
+    async logout(): Promise<any> {
         return await orbis.logout();
     }
 
-    async createPost(body: string) {
+    async createPost(body: string): Promise<any> {
         return await orbis.createPost(body);
     }
 
-    async react(post_id: number, type: string) {
+    async react(post_id: number, type: string): Promise<any> {
         return await orbis.react(post_id, type);
     }
 
-    async updateProfile(content: JSON) {
+    async updateProfile(content: JSON): Promise<any> {
         return await orbis.updateProfile(content);
     }
 
-    async setFollow(did: string, active: boolean) {
+    async setFollow(did: string, active: boolean): Promise<any> {
         return await orbis.setFollow(did, active);
     }
 
-    async createGroup(content: JSON) {
+    async createGroup(content: JSON): Promise<any> {
         return await orbis.createGroup(content);
     }
 
-    async updateGroup(stream_id: number, content: JSON) {
+    async updateGroup(stream_id: number, content: JSON): Promise<any> {
         return await orbis.updateGroup(stream_id, content);
     }
 
-    async createChannel(group_id: number, content: JSON) {
+    async createChannel(group_id: number, content: JSON): Promise<any> {
         return await orbis.createChannel(group_id, content);
     }
 
-    async updateChannel(channel_id: number, content: JSON) {
+    async updateChannel(channel_id: number, content: JSON): Promise<any> {
         return await orbis.updateChannel(channel_id, content);
     }
 
-    async createConversation(content: JSON) {
+    async createConversation(content: JSON): Promise<any> {
         return await orbis.createConversation(content);
     }
 
-    async sendMessage(content: JSON) {
+    async sendMessage(content: JSON): Promise<any> {
         return await orbis.sendMessage(content);
     }
 
-    async getPosts(content: JSON, page: number) {
+    async getPosts(content: JSON, page: number): Promise<any> {
         let { data, error } = await orbis.getPosts(content, page);
 
         return { data, error };
     }
 
-    async getPost(post_id: string) {
+    async getPost(post_id: string): Promise<any> {
         let { data, error } = await orbis.getPost(post_id);
 
         return { data, error };
     }
 
-    async getReaction(post_id: string, did: string) {
+    async getReaction(post_id: string, did: string): Promise<any> {
         let { data, error } = await orbis.getReaction(post_id, did);
 
         return { data, error };
     }
 
-    async getGroup(group_id: string) {
+    async getGroup(group_id: string): Promise<any> {
         let { data, error } = await orbis.getGroup(group_id);
 
         return { data, error };
     }
 
-    async getGroupMembers(group_id: string) {
+    async getGroupMembers(group_id: string): Promise<any> {
         let { data, error } = await orbis.getGroupMembers(group_id);
 
         return { data, error };
     }
 
-    async getIsGroupMember(group_id: string) {
+    async getIsGroupMember(group_id: string): Promise<any> {
         let { data, error } = await orbis.getIsGroupMember(group_id);
 
         return { data, error };
     }
 
 
-    async getChannel(channel_id: string) {
+    async getChannel(channel_id: string): Promise<any> {
         let { data, error } = await orbis.getChannel(channel_id);
 
         return { data, error };
     }
 
 
-    async getDids(address: string) {
+    async getDids(address: string): Promise<any> {
         let { data, error } = await orbis.getDids(address);
 
         return { data, error };
     }
 
-    async getProfile(did: string) {
+    async getProfile(did: string): Promise<any> {
         let { data, error } = await orbis.getProfile(did);
 
         return { data, error };
     }
 
-    async getProfilesByUsername(username: string) {
+    async getProfilesByUsername(username: string): Promise<any> {
         let { data, error } = await orbis.getProfilesByUsername(username);
 
         return { data, error };
     }
 
-    async getProfileGroups(did: string) {
+    async getProfileGroups(did: string): Promise<any> {
         let { data, error } = await orbis.getProfileGroups(did);
 
         return { data, error };
     }
 
-    async getIsFollowing(did_following: string, did_followed: string) {
+    async getIsFollowing(did_following: string, did_followed: string): Promise<any> {
         let { data, error } = await orbis.getIsFollowing(did_following, did_followed);
 
         return { data, error };
     }
 
-    async getProfileFollowing(did: string) {
+    async getProfileFollowing(did: string): Promise<any> {
         let { data, error } = await orbis.getProfileFollowing(did);
 
         return { data, error };
     }
 
-    async getProfileFollowers(did: string) {
+    async getProfileFollowers(did: string): Promise<any> {
         let { data, error } = await orbis.getProfileFollowers(did);
 
         return { data, error };
     }
 
-    async getConversations(options: JSON) {
+    async getConversations(options: JSON): Promise<any> {
         let { data, error } = await orbis.getConversations(options);
 
         return { data, error };
     }
 
-    async getConversation(conversation_id: string) {
+    async getConversation(conversation_id: string): Promise<any> {
         let { data, error } = await orbis.getConversation(conversation_id);
 
         return { data, error };
     }
 
-    async getMessages(conversation_id: string) {
+    async getMessages(conversation_id: string): Promise<any> {
         let { data, error } = await orbis.getMessages(conversation_id);
 
         return { data, error };
     }
 
-    async decryptMessage(content: JSON) {
+    async decryptMessage(content: JSON): Promise<any> {
         let { data, error } = await orbis.decryptMessage(content);
 
         return { data, error };
     }
 
-    async decryptPost(content: JSON) {
+    async decryptPost(content: JSON): Promise<any> {
         let { data, error } = await orbis.decryptPost(content);
 
         return { data, error };
