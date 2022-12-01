94e5d5d8971bbbd00775bff7843ec9c3cab91935
Brandon Ag <brandon.aguilarc@gmail.com>
2022-12-01 02:37:04 -0600

    adding config to protocol

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { ProfileModule } from './modules/profile/profile.module.js';
@@ -7,6 +8,14 @@ import { ProfileModule } from './modules/profile/profile.module.js';
 @Module({
   imports: [
     ProfileModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  await app.listen(configService.get('PORT'));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
