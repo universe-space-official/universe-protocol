dbae37f69d94a565767b1f1afca29bac8c678689
MartelAxe <martelaxe@gmail.com>
2023-02-10 19:36:00 -0600

    feat: adding logic to verify and decode info from the JWT

--- src/modules/profile/profile.resolver.ts
+++ src/modules/profile/profile.resolver.ts
@@ -5,6 +5,9 @@ import { fromString } from 'uint8arrays';
 import { NftService } from '../nft/nft.service.js';
 import { OrbisService } from '../orbis/orbis.service.js';
 import { Keccak } from 'sha3';
+import * as jwt from 'jsonwebtoken';
+import { decryptWithPrivateKey } from 'eth-crypto';
+
 
 import {
   ContractInformationResponse,
@@ -22,7 +25,7 @@ export class ProfileResolver {
   constructor(
     private nftService: NftService,
     private orbisService: OrbisService,
-  ) {}
+  ) { }
 
   @Query(() => NftListResponse, { name: 'getProfileNfts' })
   async getProfileNfts(@Args('address') address: string) {
@@ -121,15 +124,55 @@ export class ProfileResolver {
 
   @Mutation(() => DIDClass, { name: 'createProfile' })
   // Use a signed JWT instead of signature; Create generate JWT method
-  async createProfile(@Args('signedJWT') signedJWT: string): Promise<DIDClass> {
+  async createProfile(@Args('signedJWT') signedJWT: string, address: string): Promise<DIDClass> {
     // Receive the signedJWT and verify
-    hash.update(signedJWT);
-    hash.digest('hex');
-    const seed = new Uint8Array(fromString(hash.digest('hex'),'base16'));
-    const res = await this.orbisService.authenticateDID(seed)
-    //await this.orbisService.updateProfile(seed);
-    return {
-      did: res.did,
-    };
+
+    // @dev we are expecting this on the client side :
+    // user to sign his JWT encrypting data to our universe private key (We will make a random eth wallet for universe protocol)
+    // universe-protocol will be able to verify and decrypt the CID containing on the JWT payload
+    //////////////////////////////////////////////////////////// 
+    ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
+    // import { encryptWithPublicKey } from 'eth-crypto';
+
+    // Ethereum public key
+    // const publicKey = '0x...';
+
+    // // Data to be encrypted
+    // const data = JSON.stringify({ secret: 'My secret data' });
+
+    // // Encrypt data using public key
+    // const encryptedData = encryptWithPublicKey(publicKey, data);
+
+    // // Add encrypted data to JWT payload
+    // const payload = {
+    //   sub: '1234567890',
+    //   iat: 1577485230,
+    //   encryptedData: encryptedData
+    // };
+
+    // // Sign and encode JWT
+    // const signedJWT = jwt.sign(payload, privateKey, { algorithm: 'ES256' });
+
+    // Send JWT to universe-protocol
+    // ...
+
+    try {
+      const decoded = jwt.verify(signedJWT, address, { algorithms: ['ES256'] });
+      const universeprivatekey = '';// we will need to use a .env file or something
+      const decryptedData = decryptWithPrivateKey(universeprivatekey, decoded.encryptedData);
+      console.log('Token is valid!');
+      hash.update(decryptedData);
+      hash.digest('hex');
+      const seed = new Uint8Array(fromString(hash.digest('hex'), 'base16'));
+      const res = await this.orbisService.authenticateDID(seed)
+      //await this.orbisService.updateProfile(seed);
+      return {
+        did: res.did,
+      };
+    } catch (err) {
+      console.log('Token is invalid!');
+    }
+
   }
 }
